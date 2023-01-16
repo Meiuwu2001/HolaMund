4e92a4c32a5c70a0d031e1453482ddf81348ba53
Meiuwu2001 <samuel.reyna434@gmail.com>
2023-01-16 08:48:44 -0600

    holamundo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Holmundo';
+  title = 'Hola mundo, este es mi primer proyecto en angular, soy Samuel Reyna';
 }
